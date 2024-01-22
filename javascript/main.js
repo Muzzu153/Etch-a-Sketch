c61cb1362b16ca5ff8f613c85543b97f368bcdca
Muzaffer <muzzusyed153@gmail.com>
2024-01-23 00:12:58 +0530

    Modified the slider for the grid size
    When you click on up and down arrow the size increases and decreases by one number respectively
    you can also change the size by using the slider directly

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,53 @@
+function changeGridSize(number) {
+  let container = document.getElementById(`container`);
+  totalNumber = number * number;
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
 
-function getGridSize(number) {
-    let container = document.getElementById(`container`);
-    totalNumber = number * number;
-    container.innerHTML = ``;
+  for (let i = 0; i < totalNumber; i++) {
+    let gridSquare = document.createElement("div");
+    gridSquare.classList.add("grid_square");
 
-    for (let i = 0; i < totalNumber; i++) {
-        const gridSquare = document.createElement('div');
-        gridSquare.classList.add('grid_square');
+    let cellwidth = container.clientWidth / number + `px`;
+    let cellheight = container.clientHeight / number + `px`;
 
-        let cellwidth = container.clientWidth / number + `px`;
-        let cellheight = container.clientHeight / number + `px`;
-
-        // console.log(container);
-        gridSquare.setAttribute(`style`, `width: ${cellwidth}; height: ${cellheight};`);
-
-        container.appendChild(gridSquare);
-    }
+    gridSquare.setAttribute(
+      `style`,
+      `width: ${cellwidth}; height: ${cellheight};`
+    );
+    container.appendChild(gridSquare);
+    changeSquareBgColor();
+  }
 }
+
+function changeSquareBgColor() {
+  let color = document.getElementById(`colour_picker`);
+  let choosenColour = color.value;
+
+  color.addEventListener(`change`, function () {
+    gridSquare.style.backgroundColor = choosenColour;
+  });
+}
+
+let range = document.getElementById(`grid_range`);
+let gridSize = document.getElementById(`grid_size`);
+range.addEventListener(`change`, ()=>{
+  gridSize.textContent = range.value;
+  changeGridSize(range.value);
+})
+
+let increaseSize = document.getElementById(`arrow_up`);
+increaseSize.addEventListener(`click`, ()=> {
+  range.value++
+  gridSize.textContent = range.value;
+  changeGridSize(range.value);
+});
+
+let decreaseSize = document.getElementById(`arrow_down`);
+decreaseSize.addEventListener(`click`, ()=> {
+  range.value--
+  gridSize.textContent = range.value;
+  changeGridSize(range.value);
+});
+
